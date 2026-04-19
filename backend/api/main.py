6e2262f2652cf556f8b46eb2d31dc7dadf87f998
Morris Chen <morrischen2.9@gmail.com>
2026-04-19 23:38:54 +0800

    Fix: Try to fix the CORS problem with modifying the settings at backend(FastAPI).

--- backend/api/main.py
+++ backend/api/main.py
@@ -13,7 +13,7 @@ app.add_middleware(
     CORSMiddleware,
     # allow_origin_regex=r"^chrome-extension://.*$",
     allow_origins=["*"],  
-    allow_credentials=True,
+    allow_credentials=False,
     allow_methods=["*"],
     allow_headers=["*"],
 )
